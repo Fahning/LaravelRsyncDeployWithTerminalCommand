ef278ac9fb454781aada798acc9e4584cd6da6a3
Cícero Fahning <cicerofahning@hotmail.com>
2021-07-26 17:30:49 -0300

    novo deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,12 +9,3 @@ SERVER_DEPLOY_STRING="$REMOTE_USER@$REMOTE_HOST:$TARGET_DIRECTORY"
 
 # Run Rsync synchronization
 sh -c "rsync $ARGS -e 'ssh -i $SSHPATH/key -o StrictHostKeyChecking=no -p $REMOTE_HOST_PORT' . $SERVER_DEPLOY_STRING"
-
-
-#Run cmd command insid server
-echo "$SSHPATH/key $SERVER_DEPLOY_STRING -p $REMOTE_HOST_PORT"
-ssh "$SSHPATH/key $SERVER_DEPLOY_STRING -p $REMOTE_HOST_PORT"
-
-cd "$TARGET_DIRECTORY" && php artisan optimize
-echo date /t >> logDeploy.txt
-echo "Comando pos deploy executado" >> logDeploy.txt
